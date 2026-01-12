828ca496f3e3be626c6e675fe173bd2e115b007f
Gal Harel <galq@post.bgu.ac.il>
2026-01-12 17:09:46 +0200

    MAIN: Enable server type selection and argument parsing in StompServer
    Uncommented and refactored the main method to support command-line argument parsing for port and server type (tpc or reactor). Added error handling for invalid arguments and enabled server instantiation based on the selected type.

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -2,43 +2,45 @@ package bgu.spl.net.impl.stomp;
 
 import bgu.spl.net.srv.Server;
 import bgu.spl.net.srv.Reactor;
-import bgu.spl.net.api.StompMessagingProtocolImpl;
-import bgu.spl.net.api.MessageEncoderDecoder;
+
 
 public class StompServer {
 
     public static void main(String[] args) {
-        // if (args.length < 2) {
-        //     System.err.println("Usage: java StompServer <port> <tpc|reactor> [numThreads]");
-        //     System.exit(1);
-        // }
+        if (args.length < 2) {
+            System.out.println("Usage: StompServer <port> <tpc|reactor>");
+            return;
+        }
+        
+        int port;
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid port number: " + args[0]);
+            return;
+        }
+        
+        String serverType = args[1];
 
-        // try {
-        //     int port = Integer.parseInt(args[0]);
-        //     String serverType = args[1];
-        //     Server<String> server;
-        //     int numOfThreads = args.length > 2 ? Integer.parseInt(args[2]) : 4;
-
-        //     if (serverType.equals("tpc")) {
-        //         server = new TPCServer<String>(
-        //                 port,
-        //                 () -> new StompMessagingProtocolImpl(),
-        //                 () -> new StompEncoderDecoder());
-        //         server.serve();
-        //     } else if (serverType.equals("reactor")) {
-        //         server = new Reactor<String>(
-        //                 numOfThreads,
-        //                 port,
-        //                 () -> new StompMessagingProtocolImpl(),
-        //                 () -> new StompEncoderDecoder());
-        //         server.serve();
-        //     } else {
-        //         System.err.println("Invalid server type. Use 'tpc' or 'reactor'");
-        //         System.exit(1);
-        //     }
-        // } catch (NumberFormatException e) {
-        //     System.err.println("Invalid port or thread count: " + e.getMessage());
-        //     System.exit(1);
-        // }
+        if (serverType.equals("tpc")) {
+            new TPCServer<>(
+                    port,
+                    () -> new StompMessagingProtocolImpl(), // Protocol factory
+                    () -> new StompEncoderDecoder()         // Encoder factory
+            ).serve();
+        } 
+        
+        else if (serverType.equals("reactor")) {
+            new Reactor<>(
+                    2, 
+                    port,
+                    () -> new StompMessagingProtocolImpl(), // Protocol factory
+                    () -> new StompEncoderDecoder()         // Encoder factory
+            ).serve();
+        } 
+        
+        else {
+            System.out.println("Unknown server type: " + serverType);
+        }
     }
 }
