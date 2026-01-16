00f67c5dc178876c7ff131f445c43bcd88745b00
Gal Harel <galq@post.bgu.ac.il>
2026-01-16 16:41:49 +0200

    (cout) Add connection status messages to StompClient
    Added console output to indicate connection progress, protocol initialization, thread joining, and program exit in main(). This improves user feedback during client startup and shutdown.

--- client/src/StompClient.cpp
+++ client/src/StompClient.cpp
@@ -21,16 +21,18 @@ int main(int argc, char *argv[]) {
     short port = atoi(argv[2]);
     
     ConnectionHandler connectionHandler(host, port);
+    cout << "Connecting..." << endl;
     if (!connectionHandler.connect()) {
         cerr << "Cannot connect to " << host << ":" << port << endl;
         return 1;
     }
+    cout << "Connected!" << endl;
 
     StompProtocol protocol(&connectionHandler);
+    cout << "Protocol initialized" << endl;
 
     // Thread 2: Socket Listener
     thread socketThread([&connectionHandler, &protocol]() {
-        // an infinite loop, using non-zero integer as a boolean
         while (1) { 
             string frame;
             // Get frame until null character
@@ -55,7 +57,9 @@ int main(int argc, char *argv[]) {
     }
     
     // Cleanup
+    cout << "Joining socket thread..." << endl;
     if (socketThread.joinable()) socketThread.join();
     
+    cout << "Exiting main..." << endl;
     return 0;
 }
