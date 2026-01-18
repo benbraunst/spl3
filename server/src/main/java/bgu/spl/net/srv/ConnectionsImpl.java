5527361f1d72ea5af4a117f6e5e35677f7f41dfd
benbr <braunstein.ben@gmail.com>
2026-01-18 16:49:58 +0200

    Corrected message string logic, added debug prints

--- server/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
+++ server/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
@@ -25,21 +25,28 @@ public class ConnectionsImpl<T> implements Connections<T> {
     @Override
     public void send(String channel, T msg) {
         ConcurrentHashMap<Integer, Integer> subscribers = channelSubscriptions.get(channel);
+        System.out.println("[ConnectionsImpl] Broadcasting to channel '" + channel + "'");
+        
         if (subscribers != null) {
-            // Create base message once (assumes T is String for STOMP protocol)
-            String baseMessage = "MESSAGE\n" +
-                               "subscription:%d\n" +
-                               "message-id:" + java.util.UUID.randomUUID().toString() + "\n" +
-                               "destination:" + channel + "\n" +
-                               "\n" +
-                               msg;
+            System.out.println("[ConnectionsImpl] Channel has " + subscribers.size() + " subscriber(s)");
+            
+            // Create headers template once (without body to avoid String.format issues)
+            String messageId = java.util.UUID.randomUUID().toString();
+            String headerTemplate = "MESSAGE\n" +
+                                   "subscription:%d\n" +
+                                   "message-id:" + messageId + "\n" +
+                                   "destination:" + channel + "\n" +
+                                   "\n";
             
             // Use entrySet() to avoid race condition between get() calls
             for (java.util.Map.Entry<Integer, Integer> entry : subscribers.entrySet()) {
                 Integer connectionId = entry.getKey();
                 Integer subscriptionId = entry.getValue();
                 if (subscriptionId != null) {
-                    String personalizedMsg = String.format(baseMessage, subscriptionId);
+                    // Format only the header part, then append body
+                    String headers = String.format(headerTemplate, subscriptionId);
+                    String personalizedMsg = headers + msg;
+                    System.out.println("[ConnectionsImpl] Sending MESSAGE to client " + connectionId + " (subscription ID: " + subscriptionId + ")");
                     send(connectionId, (T) personalizedMsg);
                 }
             }
@@ -51,15 +58,24 @@ public class ConnectionsImpl<T> implements Connections<T> {
         ConnectionHandler<T> handler = activeConnections.get(connectionId);
 
         if (handler != null) {
+            System.out.println("[ConnectionsImpl] Disconnecting client " + connectionId);
             activeConnections.remove(connectionId);
 
             // Remove from all channel subscriptions
+            int totalUnsubscribed = 0;
             for (String channel : channelSubscriptions.keySet()) {
                 ConcurrentHashMap<Integer, Integer> subscribers = channelSubscriptions.get(channel);
                 if (subscribers != null) {
-                    subscribers.remove(connectionId);
+                    Integer subId = subscribers.remove(connectionId);
+                    if (subId != null) {
+                        totalUnsubscribed++;
+                        System.out.println("[ConnectionsImpl] Removed client " + connectionId + " from channel '" + channel + "'");
+                    }
                 }
             }
+            System.out.println("[ConnectionsImpl] Client " + connectionId + " unsubscribed from " + totalUnsubscribed + " channel(s)");
+        } else {
+            System.out.println("[ConnectionsImpl] WARNING: Attempted to disconnect unknown client " + connectionId);
         }
     }
 
@@ -69,15 +85,25 @@ public class ConnectionsImpl<T> implements Connections<T> {
 
     @Override
     public void subscribe(String channel, int connectionId, int subscriptionId) {
+        System.out.println("[ConnectionsImpl] Subscribing client " + connectionId + " to channel '" + channel + "' with subscription ID " + subscriptionId);
         channelSubscriptions.putIfAbsent(channel, new ConcurrentHashMap<>());
         channelSubscriptions.get(channel).put(connectionId, subscriptionId);
+        System.out.println("[ConnectionsImpl] Channel '" + channel + "' now has " + channelSubscriptions.get(channel).size() + " subscriber(s)");
     }
 
     @Override
     public void unsubscribe(String channel, int connectionId) {
         ConcurrentHashMap<Integer, Integer> channelSubscribers = channelSubscriptions.get(channel);
         if (channelSubscribers != null) {
-            channelSubscribers.remove(connectionId);
+            Integer removedSubId = channelSubscribers.remove(connectionId);
+            if (removedSubId != null) {
+                System.out.println("[ConnectionsImpl] Unsubscribed client " + connectionId + " from channel '" + channel + "' (subscription ID was " + removedSubId + ")");
+                System.out.println("[ConnectionsImpl] Channel '" + channel + "' now has " + channelSubscribers.size() + " subscriber(s)");
+            } else {
+                System.out.println("[ConnectionsImpl] WARNING: Client " + connectionId + " was not subscribed to channel '" + channel + "'");
+            }
+        } else {
+            System.out.println("[ConnectionsImpl] WARNING: Channel '" + channel + "' does not exist");
         }
     }
 }
